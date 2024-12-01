22e7dde0d9080f5910523053dab2019d9d440d5b
hakantulgac <hakantulgac@gmail.com>
2024-12-01 19:22:55 +0300

    Update for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const os = require("os");
-var cors = require('cors')
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,18 +10,8 @@ const hostname = os.hostname();
 app.use(cors());
 app.use(express.json());
 
-const data = require("./public/data/target-data.json")
-
-app.get("/data",(req,res)=>{
-    res.send(data);
-});
-
-app.get("/vehicleImage/:file_name",(req, res)=>{
-    const fileName = req.params.file_name;
-    const fileDir = `${__dirname}/public/images/vehicles/${fileName}`;
-
-    res.sendFile(fileDir);
-});
+const routes = require("./routes");
+app.use("/", routes);
 
 app.listen(port, () => {
     console.log(`Server running at ${hostname}:${port}`);
